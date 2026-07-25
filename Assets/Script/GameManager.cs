550ab16001b591cbe2416883f4d2aa943deee78c
JiraJiran <Jirajiran.l@gmail.com>
2026-07-25 16:02:38 +0700

    Update7_25_26

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,9 +1,17 @@
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     
     [SerializeField] private int playerScore;
+
+    [SerializeField]
+    private GameObject[] ballPositions;
+
+    [SerializeField]
+    private GameObject ballPrefab;
     public int PlayerScore { get { return playerScore; } set { playerScore = value; } }
 
     public static GameManager instance;
@@ -15,6 +23,22 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        SetBall(BallColor.White, 0);
+        SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Yellow, 2);
+        SetBall(BallColor.Green, 3);
+        SetBall(BallColor.Brown, 4);
+        SetBall(BallColor.Blue, 5);
+        SetBall(BallColor.Pink, 6);
+        SetBall(BallColor.Black, 7);
+
+    }
+    private void SetBall(BallColor colorB, int i)
+    {
+
+        GameObject obj = Instantiate(ballPrefab,ballPositions[i].transform.position, Quaternion.identity);
+        ball b = obj.GetComponent<ball>();
+        b.SetColorAndPoint(colorB);
+
     }
 }
--- Assets/Script/ball.cs
+++ Assets/Script/ball.cs
@@ -14,13 +14,22 @@ public enum BallColor
 }
 public class ball : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField]private int point;
+    [SerializeField]
+    private int point;
 
-    [SerializeField]private BallColor color;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    private BallColor colorB;
+
+    private MeshRenderer mr;
+
+
+    private void Awake()
+    {
+        mr = GetComponent<MeshRenderer>();
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -43,5 +52,44 @@ public class ball : MonoBehaviour, IPointerClickHandler
         //        GameManager.instance.PlayerScore += point;
         //        break;
         //}
+    }
+
+    public void SetColorAndPoint(BallColor colorB)
+    {
+       switch(colorB)
+        {
+            case BallColor.White:
+                point = 0;
+                mr.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                mr.material.color = Color.red;
+                break;
+            case BallColor.Yellow:
+                point = 2;
+                mr.material.color = Color.yellow;
+                break;
+            case BallColor.Green:
+                point = 3;
+                mr.material.color = Color.green;
+                break;
+            case BallColor.Brown:
+                point = 4;
+                mr.material.color = new Color(0.6f, 0.3f, 0.1f);
+                break;
+            case BallColor.Blue:
+                point = 5;
+                mr.material.color = Color.blue;
+                break;
+            case BallColor.Pink:
+                point = 6;
+                mr.material.color = new Color(1f, 0.4f, 0.7f);
+                break;
+            case BallColor.Black:
+                point = 7;
+                mr.material.color = Color.black;
+                break;
+        }
     }
 }
